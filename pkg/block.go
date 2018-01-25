706028b9d0b6034d773029b773db88633d6ea1cc
Sandeep Raju Prabhakar <me@sandeepraju.in>
2018-01-25 00:24:38 -0800

    fixes and helpers

--- pkg/block.go
+++ pkg/block.go
@@ -2,18 +2,42 @@ package pkg
 
 import "time"
 
+const (
+	// BlockVersion ...
+	BlockVersion = 0
+)
+
 // BlockHeader ...
 type BlockHeader struct {
 	Version    string
 	PrevBlock  string // Hash of previous block
 	MerkleRoot string // Hash of merkle root
 	Time       time.Time
-	Target     uint64
-	Nonce      uint64
+	Target     uint32
+	Nonce      uint32
 }
 
 // Block ...
 type Block struct {
 	BlockHeader
 	Transactions []Transaction // First transaction in this is the transaction to gift the miner
+}
+
+// IsGenesis ...
+func (b *Block) IsGenesis() bool {
+	if b.PrevBlock == "" {
+		return true
+	}
+
+	return false
+}
+
+// NewBlock ...
+func NewBlock() *Block {
+	return &Block{}
+}
+
+// NewGenesisBlock ...
+func NewGenesisBlock() *Block {
+	return &Block{}
 }
